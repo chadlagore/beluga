cf5b8e74f92389d45a107f3551b36dde99648008
chadlagore <chad.d.lagore@gmail.com>
2017-10-02 17:26:19 -0700

    App initialization

--- beluga/app.py
+++ beluga/app.py
@@ -1,1 +1,13 @@
-print("Hello, World!!!!!!")
+from aiohttp import web
+from routes import setup_routes
+
+
+# Create the application.
+app = web.Application()
+
+# Setup our routes.
+setup_routes(app)
+
+# Run the app.
+if __name__ == '__main__':
+    web.run_app(app, host='0.0.0.0', port=80)
