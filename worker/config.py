d02bf08fafd46a2ffbca9a6d1e7e1340f1a0ba78
chadlagore <chad.d.lagore@gmail.com>
2017-10-11 19:43:37 -0700

    Newlines

--- worker/config.py
+++ worker/config.py
@@ -1,8 +1,5 @@
 import os
 
-
-CELERY_BROKER_URL = os.environ['CELERY_BROKER_URL']
-
 # Geo config (this is an odd place to put this isnt it).
 VANCOUVER_LAT = 49.241
 VANCOUVER_LON = -123.1073
@@ -11,6 +8,7 @@ VANCOUVER_RAD = "10km"
 # Celery eats this stuff automagically.
 timezone = 'America/Vancouver'
 result_backend = os.environ['CELERY_RESULT_BACKEND']
+CELERY_BROKER_URL = os.environ['CELERY_BROKER_URL']
 
 # Eventbrite things.
 EVENTBRITE_APP_KEY = os.environ['EVENTBRITE_APP_KEY']
