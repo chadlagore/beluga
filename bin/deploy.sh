8521910370ca9ab46c80af01ae5eb2fd54d5da5c
chadlagore <chad.d.lagore@gmail.com>
2017-10-12 20:49:46 -0700

    Using heroku command line app

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
+
+# This is the deploy target.
 TARGET=$1
 
+set -e # Abort script at first error
+set -u # Disallow unset variables
+
 if [ "$TARGET" = "heroku" ]; then
+
+    # Install the toolbelt, and the required plugin.
+    wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+    heroku plugins:install heroku-docker
+
+    # Heroku likes this approach.
     cp Dockerfile Dockerfile.web
     cp worker/Dockerfile Dockerfile.worker
+
+    # Push all containers to registry
     heroku container:push web worker --recursive
-    rm Dockerfile.web
-    rm Dockerfile.worker
+
 elif [ "$TARGET" = "gcp" ]; then
     echo "GCP deployment not implemented"
 else
